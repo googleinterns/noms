154ba8655b1b680a140cd09d46e8eff551754679
Melanie Gutzmann <mirrorkeydev@gmail.com>
2020-07-21 19:23:00 +0000

    Add bare bones map

---
+++ src/main/webapp/script.js
@@ -0,0 +1,8 @@
+console.log("js loaded!")
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8
+  });
+}
