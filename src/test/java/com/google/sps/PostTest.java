f30597df877dca917c15ddd07dff5beaaae73d58
ubahl <ubahl@scu.edu>
2020-08-12 17:25:30 +0000

    Sets up testing files.

---
+++ src/test/java/com/google/sps/PostTest.java
@@ -0,0 +1,13 @@
+package com.google.sps;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public final class PostTest {
+  
+  
+}
