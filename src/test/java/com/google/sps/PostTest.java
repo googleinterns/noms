cd221a940bb131f3c57ce60d3e0afcf01da816fe
ubahl <ubahl@scu.edu>
2020-08-13 16:18:58 +0000

    Wrote second test.

--- src/test/java/com/google/sps/PostTest.java
+++ src/test/java/com/google/sps/PostTest.java
@@ -13,7 +13,7 @@ import static org.mockito.Mockito.*;
 @RunWith(JUnit4.class)
 public final class PostTest {
 
-  public void testRequestToPostBasic(String collegeId, String organizationName, int month, int day, int startHour, int startMinute, String startAMorPM, int endHour, 
+  public void testRequestToPost(String collegeId, String organizationName, int month, int day, int startHour, int startMinute, String startAMorPM, int endHour, 
   int endMinute, String endAMorPM, String location, Double lat, Double lng, String numberOfPeopleItFeeds, String typeOfFood, String description) {
 
     // Construct Mock HTTP Request
@@ -93,7 +93,57 @@ public final class PostTest {
 
   // Test basic functionality of requestToPost(): an event in the morning
   @Test
-  public void testRequestToPostBasic() {
+  public void testRequestToPostAM() {
+
+    String collegeId = "122931";
+    String organizationName = "SWE";
+    int month = 8;
+    int day = 24;
+    int startHour = 4;
+    int startMinute = 30;
+    String startAMorPM = "am";
+    int endHour = 5;
+    int endMinute = 00;
+    String endAMorPM = "am";
+    String location = "Benson Memorial Center";
+    Double lat = 37.3476132;
+    Double lng = -121.9394005;
+    String numberOfPeopleItFeeds = "20";
+    String typeOfFood = "Chocolate cake";
+    String description = "Birthday Party!!!";
+
+    testRequestToPost(collegeId, organizationName, month, day, startHour, startMinute, startAMorPM, endHour, 
+    endMinute, endAMorPM, location, lat, lng, numberOfPeopleItFeeds, typeOfFood, description);
+  }
+
+  // Test basic functionality of requestToPost(): an event in the morning ending in the afternoon
+   @Test
+  public void testRequestToPostAMandPM() {
+
+    String collegeId = "110653";
+    String organizationName = "WICS";
+    int month = 9;
+    int day = 01;
+    int startHour = 11;
+    int startMinute = 00;
+    String startAMorPM = "am";
+    int endHour = 1;
+    int endMinute = 00;
+    String endAMorPM = "pm";
+    String location = "Aldrich Park";
+    Double lat = 33.6460519;
+    Double lng = -117.8427446;
+    String numberOfPeopleItFeeds = "5";
+    String typeOfFood = "Pizza";
+    String description = "We ordered too much pizza! Drop by during our workshop to pick it up";
+
+    testRequestToPost(collegeId, organizationName, month, day, startHour, startMinute, startAMorPM, endHour, 
+    endMinute, endAMorPM, location, lat, lng, numberOfPeopleItFeeds, typeOfFood, description);
+  }
+
+  // Test basic functionality of requestToPost(): an event in the evening
+  @Test
+  public void testRequestToPostPM() {
 
     String collegeId = "122931";
     String organizationName = "SWE";
@@ -112,42 +162,15 @@ public final class PostTest {
     String typeOfFood = "Chocolate cake";
     String description = "Birthday Party!!!";
 
-    testRequestToPostBasic(collegeId, organizationName, month, day, startHour, startMinute, startAMorPM, endHour, 
+    testRequestToPost(collegeId, organizationName, month, day, startHour, startMinute, startAMorPM, endHour, 
     endMinute, endAMorPM, location, lat, lng, numberOfPeopleItFeeds, typeOfFood, description);
   }
-
-  // Test basic functionality of requestToPost(): an event in the morning ending in the afternoon
-    @Test
-  public void testRequestToPostBasic() {
-
-    String collegeId = "122931";
-    String organizationName = "SWE";
-    int month = 8;
-    int day = 24;
-    int startHour = 4;
-    int startMinute = 30;
-    String startAMorPM = "am";
-    int endHour = 5;
-    int endMinute = 0;
-    String endAMorPM = "pm";
-    String location = "Benson Memorial Center";
-    Double lat = 37.3476132;
-    Double lng = -121.9394005;
-    String numberOfPeopleItFeeds = "20";
-    String typeOfFood = "Chocolate cake";
-    String description = "Birthday Party!!!";
-
-    testRequestToPostBasic(collegeId, organizationName, month, day, startHour, startMinute, startAMorPM, endHour, 
-    endMinute, endAMorPM, location, lat, lng, numberOfPeopleItFeeds, typeOfFood, description);
-  }
-
-  // Test basic functionality of requestToPost(): an event in the evening
 
   // No tests needed for queryToPost, etc bc google api
   // Invalid options tested in javascript
 
   // Test basic functionality: postToEntity()
 
-  
+  // ENd month, beginning month
   
 }
